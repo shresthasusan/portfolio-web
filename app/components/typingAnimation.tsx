499d4f947a283cc0bd0898746f8fbb6ad0377cc7
shresthasusan <shresthasusan44@gmail.com>
2024-12-25 19:54:18 +0545

    Update styles, add components, and implement typing animation

---
+++ app/components/typingAnimation.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { useEffect, useState } from "react";
+
+interface Props {
+  className?: string | null;
+  text: string;
+  startDelay?: number;
+  interval: number;
+}
+
+const TypingAnimation = ({ className, text, startDelay, interval }: Props) => {
+  const [typedText, setTypedText] = useState<string>("");
+
+  useEffect(() => {
+    let localTypingIndex = 0;
+    let localTyping = "";
+
+    const startTimeout = setTimeout(() => {
+      const printer = setInterval(() => {
+        if (localTypingIndex < text.length) {
+          localTyping += text[localTypingIndex];
+          setTypedText(localTyping);
+          localTypingIndex += 1;
+        } else {
+          clearInterval(printer);
+        }
+      }, interval);
+
+      return () => clearInterval(printer); // Cleanup the interval on component unmount
+    }, startDelay);
+
+    return () => clearInterval(startTimeout); // Cleanup the interval on component unmount
+  }, [text, startDelay]);
+
+  return <span className={`${className}`}>{typedText}</span>;
+};
+
+export default TypingAnimation;
--- app/root/page.tsx
+++ app/root/page.tsx
@@ -1,8 +1,31 @@
 import Image from "next/image";
+import React from "react";
+import TypingAnimation from "../components/typingAnimation";
 
-export default function Home() {
+const page = () => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="mt-64">
+        <div className="text-white text-8xl">
+          <p>Hi!</p>
+          <p>I'm</p>
+          <p>
+            <TypingAnimation text={"Susan"} interval={146}></TypingAnimation>
+          </p>
+          <p>
+            <TypingAnimation
+              text="Shrestha"
+              startDelay={900}
+              interval={146}
+            ></TypingAnimation>
+            <TypingAnimation
+              text="."
+              startDelay={2500}
+              interval={146}
+            ></TypingAnimation>
+          </p>
+        </div>
+      </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -52,4 +75,6 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+};
+
+export default page;
