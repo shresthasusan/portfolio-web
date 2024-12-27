2a34cca01b2332a1b35e400f4cce19d1ed9c28df
shresthasusan <shresthasusan44@gmail.com>
2024-12-28 00:08:09 +0545

    Add meta tags and title to improve document structure in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Susan Shrestha</title>
+      </head>
       <body className={` ${poppins.className} antialiased flex`}>
         {children}
       </body>
