93e8240c8bd98ea2994c2a8621daceeebd036a79
shresthasusan <shresthasusan44@gmail.com>
2025-01-03 08:14:15 +0545

    Update SideNav component with styling for improved layout and responsiveness

--- app/components/sideNav.tsx
+++ app/components/sideNav.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SideNav = () => {
-  return <div>SideNav</div>;
+  return <div className="side-nav bg-black sticky top-0 md:w-16 w-8"></div>;
 };
 
 export default SideNav;
